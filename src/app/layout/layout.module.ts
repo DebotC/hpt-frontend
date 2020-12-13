b8c90b418477b646c92d819d97dcc80e22e5f762
DebotC <cedric.debot@gmail.com>
2020-12-13 22:08:33 +0100

    Header

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [LayoutComponent, HeaderComponent],
+  exports: [LayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
